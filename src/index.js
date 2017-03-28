04b15f218aa1e17d4f8b9f76660e6b4748f53ab8
asd <asd>
2017-03-28 18:03:56 +0300

    dasdasd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 console.log("Hello world");
+
+var express = require('express');
+var app = express();
+var fetch = require('node-fetch');
+var cheerio =require('cheerio');
+var url = 'http://www.dopopoco.ro/meniu-individual-timisoara';
+
+function helloHanlder(req, res){
+  var dopopocoResponse = fetch(url);
+  var dopopocoText = dopopocoResponse.then(function (dopopocoHtml){
+    return dopopocoHtml.text();
+  });
+
+function procesText(text) {
+  var $ = cheerio.load(text);
+
+  var listItem = Array.form($("#tiles").children("li"));
+  return listItem.map(function (li) {
+    return{
+      title: $("li").find(".tiles").text()
+    };
+  });
+
+}
+  dopopocoText.then(function(theText){
+    res.send(procesText(theText));
+  });
+
+}
+
+app.get('/', helloHanlder);
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
