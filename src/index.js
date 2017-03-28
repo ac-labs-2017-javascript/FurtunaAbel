0488b40446fe34aad2cc6fe4ac882d1062f08dcc
adriancreteanu <creteanu.adrian@gmail.com>
2017-03-28 18:03:24 +0300

    stuff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("Hello world");
+var express = require("express");
+var fetch = require("node-fetch"); //call catre url, primim datele 
+
+var cheerio = require("cheerio");
+var app = express();
+
+
+// http://www.dopopoco.ro/meniu-individual-timisoara
+
+function processText(){
+  var $ = cheerio.load(text);
+
+  var listItems = Array.from($("#tiles").children("li"));
+  return listItems.map(function(li){
+  	return{
+  		title: $(li).find(".title").text()
+  	};
+  });
+}
+
+
+app.get("/hello",function(req,res){
+	//res.send("Bye world");
+	var dopopocoSite = fetch("http://www.dopopoco.ro/meniu-individual-timisoara"); //returneaza un obj
+	var textResponse = dopopocoSite.then(function(response){
+// response -> contentul url-ului
+      return response.text();
+	});
+	textResponse.then(function(text){
+		res.send(processText(text)); //res ajunge la browser
+	});
+});
+
+app.listen(8090,function(){
+	console.log("Server running on 8090");
+});
